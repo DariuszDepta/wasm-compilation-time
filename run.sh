8849e06f927108d0abd088728dc8a11ba2dbfb3e
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2026-03-04 17:10:53 +0100

    Updates.

--- run.sh
+++
@@ -1,9 +0,0 @@
-cargo build --release
-
-#./target/release/compilation-time wasmer-cranelift-none
-#./target/release/compilation-time wasmer-cranelift-speed
-#./target/release/compilation-time wasmer-singlepass
-#./target/release/compilation-time wasmtime-cranelift-none
-#./target/release/compilation-time wasmtime-cranelift-speed
-#./target/release/compilation-time wasmtime-singlepass
-./target/release/compilation-time cosmwasm-singlepass
