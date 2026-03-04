f0037281c1d002b4086ff49890f7389e86d530a5
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2026-03-04 13:16:01 +0100

    Updates.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 use std::collections::BTreeMap;
+use std::fmt::Write;
 use std::path::{Path, PathBuf};
 use std::time::{Duration, Instant};
 use walkdir::WalkDir;
 
 const WASM_BINARIES_DIR: &str = "neutron_wasm_codes";
+const RESULTS_DIR: &str = "results";
 
 fn get_code(path: impl AsRef<Path>) -> Vec<u8> {
     std::fs::read(path).expect("failed to load WASM file")
 }
 
+fn write_file(file_name: &str, content: &str) {
+    let path = Path::new(RESULTS_DIR).join(file_name);
+    std::fs::write(path, content).expect("failed to write output file");
+}
+
 fn print_result(
+    success: &mut dyn Write,
+    failure: &mut dyn Write,
     is_ok: bool,
     path: impl AsRef<Path>,
     size: usize,
@@ -19,41 +28,61 @@ fn print_result(
     let file = path.as_ref().file_name().unwrap().to_string_lossy();
     if is_ok {
         println!("{:>12} {:>20} {:>20}", file, size, duration.as_nanos());
+        writeln!(
+            success,
+            "{:>12} {:>20} {:>20}",
+            file,
+            size,
+            duration.as_nanos()
+        )
+        .unwrap();
     } else {
         eprintln!(
             "{:>12} {:>20} {}",
             file,
             size,
-            error.unwrap_or("compilation error".to_string())
+            error.clone().unwrap_or("compilation error".to_string())
         );
+        writeln!(
+            failure,
+            "{:>12} {:>20} {}",
+            file,
+            size,
+            error.unwrap_or("compilation error".to_string())
+        )
+        .unwrap();
     }
 }
 
-fn compile_using_wasmer<T>(files: T, singlepass: bool, speed: bool)
+fn use_wasmer<T>(profile: &str, files: T, singlepass: bool, speed: bool)
 where
     T: Iterator<Item = PathBuf>,
 {
+    let store = if singlepass {
+        let compiler = wasmer::sys::Singlepass::default();
+        wasmer::Store::new(compiler)
+    } else {
+        let mut compiler = wasmer::sys::Cranelift::default();
+        if speed {
+            compiler.opt_level(wasmer::sys::CraneliftOptLevel::Speed);
+        } else {
+            compiler.opt_level(wasmer::sys::CraneliftOptLevel::None);
+        }
+        wasmer::Store::new(compiler)
+    };
+
+    let mut success = String::new();
+    let mut failure = String::new();
     for file in files {
         let code = get_code(&file);
-
-        let store = if singlepass {
-            let compiler = wasmer::sys::Singlepass::default();
-            wasmer::Store::new(compiler)
-        } else {
-            let mut compiler = wasmer::sys::Cranelift::default();
-            if speed {
-                compiler.opt_level(wasmer::sys::CraneliftOptLevel::Speed);
-            } else {
-                compiler.opt_level(wasmer::sys::CraneliftOptLevel::None);
-            }
-            wasmer::Store::new(compiler)
-        };
 
         let start = Instant::now();
         let result = wasmer::Module::new(&store, &code);
         let duration = start.elapsed();
 
         print_result(
+            &mut success,
+            &mut failure,
             result.is_ok(),
             file,
             code.len(),
@@ -61,14 +90,15 @@ where
             result.err().map(|e| e.to_string()),
         );
     }
+    write_file(&format!("{}.txt", profile), &success);
+    write_file(&format!("{}-err.txt", profile), &failure);
 }
 
-fn compile_using_wasmtime<T>(files: T, singlepass: bool, speed: bool)
+fn use_wasmtime<T>(profile: &str, files: T, singlepass: bool, speed: bool)
 where
     T: Iterator<Item = PathBuf>,
 {
     let mut config = wasmtime::Config::new();
-
     if singlepass {
         config.strategy(wasmtime::Strategy::Winch);
     } else {
@@ -79,11 +109,11 @@ where
             config.cranelift_opt_level(wasmtime::OptLevel::None);
         }
     }
-
     config.parallel_compilation(true);
-
     let engine = wasmtime::Engine::new(&config).expect("failed to instantiate engine");
 
+    let mut success = String::new();
+    let mut failure = String::new();
     for file in files {
         // Load the binary to memory.
         let code = get_code(&file);
@@ -93,6 +123,8 @@ where
         let duration = start.elapsed();
 
         print_result(
+            &mut success,
+            &mut failure,
             result.is_ok(),
             file,
             code.len(),
@@ -100,6 +132,8 @@ where
             result.err().map(|e| e.to_string()),
         );
     }
+    write_file(&format!("{}.txt", profile), &success);
+    write_file(&format!("{}-err.txt", profile), &failure);
 }
 
 fn main() {
@@ -123,12 +157,12 @@ fn main() {
     }
 
     match args[0].as_str() {
-        "wasmer-cranelift-none" => compile_using_wasmer(files.values().cloned(), false, false),
-        "wasmer-cranelift-speed" => compile_using_wasmer(files.values().cloned(), false, true),
-        "wasmer-singlepass" => compile_using_wasmer(files.values().cloned(), true, false),
-        "wasmtime-cranelift-none" => compile_using_wasmtime(files.values().cloned(), false, false),
-        "wasmtime-cranelift-speed" => compile_using_wasmtime(files.values().cloned(), false, true),
-        "wasmtime-singlepass" => compile_using_wasmtime(files.values().cloned(), true, false),
+        p @ "wasmer-cranelift-none" => use_wasmer(p, files.values().cloned(), false, false),
+        p @ "wasmer-cranelift-speed" => use_wasmer(p, files.values().cloned(), false, true),
+        p @ "wasmer-singlepass" => use_wasmer(p, files.values().cloned(), true, false),
+        p @ "wasmtime-cranelift-none" => use_wasmtime(p, files.values().cloned(), false, false),
+        p @ "wasmtime-cranelift-speed" => use_wasmtime(p, files.values().cloned(), false, true),
+        p @ "wasmtime-singlepass" => use_wasmtime(p, files.values().cloned(), true, false),
         _ => eprintln!("error: invalid argument"),
     }
 }
